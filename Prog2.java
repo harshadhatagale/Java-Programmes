28b056ce6eff3708903f114a92e66d15d3823ae2
harshadhatagale <harshadhatagale6@gmail.com>
2025-08-04 21:36:44 +0530

    Init

---
+++ Prog2.java
@@ -0,0 +1,30 @@
+
+public class Argumenrest {
+public static void main (String
+new Test ( ) ;
+Test t
+t. start ( ) ;
+static class Test {
+void start ( )
+int a
+4;
+int b
+s;
+System. out
+System. out
+Sys tem. out
+System. out
+System. out
+Sys tem. out
+String foo ( )
+. print (
+. print (a + b);
+. print '
+. print (foo ( ) +
+. print In (a + b
+. print (a + b);
+args)
+foo());
+return " foo" ;
+}
+}
